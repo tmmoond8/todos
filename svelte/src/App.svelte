f2f1828743a35c69fc7f2f63b63c2a4ec4876c90
tmmoond8 <tmmoond8@gmail.com>
2021-12-02 21:48:58 +0900

    🎉 스벨트 공부 시작 기본 컴포넌트 작성

---
+++ svelte/src/App.svelte
@@ -0,0 +1,84 @@
+<script>
+	export const todos = [
+		{ id: '123323213', content: '스벨트로 TODO 만들기', complteted: true }
+	];
+	export let inputText;
+
+	function addTodo(content) {
+		todos.push({
+			id: 'asd2s',
+			content,
+			complteted: false,
+		})
+		todos = todos;
+		inputText = '';
+	}
+
+	function toggle(index) {
+		todos[index].complteted = !todos[index].complteted;
+		todos = todos;
+	}
+</script>
+
+<main>
+	<h1>Svelte를 사용한 Todo App 코드 입니다.</h1>
+	<div class="input-form">
+		<button on:click={() => addTodo(inputText)}>add</button>
+		<input type="text" bind:value={inputText} on:keydown={(e) => {
+			if (e.key === 'Enter' && inputText.length > 0) {
+				addTodo(inputText)
+			}
+		}}/>
+	</div>
+	<ol>
+		{#each todos as todo, index }
+			<li on:click|preventDefault={() => toggle(index)}>
+				<label for={todo.id} class={todo.complteted ? "completed" : ""} >
+					<input type="checkbox" id={todo.id} checked={todo.complteted}/> {todo.content}
+				</label>
+			</li>
+		{/each}
+	</ol>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	.input-form {
+		display: flex;
+		justify-content: center;
+	}
+
+	ol {
+		max-width: 400px;
+		list-style: disc;
+		margin: 20px auto 0 auto;
+		text-align: left;
+	}
+
+	li + li {
+		margin-top: 6px;
+	}
+
+	label.completed {
+		text-decoration: line-through;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
