c2a7eca38608ed352b151136d536ac214f46abf2
tmmoond8 <tmmoond8@gmail.com>
2022-01-23 14:17:52 +0900

    ✨ 다크 테마 적용

--- react-chakra-ui/src/index.js
+++ react-chakra-ui/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import theme from "./theme";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </BrowserRouter>,
   rootElement
--- react-chakra-ui/src/pages/Home.js
+++ react-chakra-ui/src/pages/Home.js
@@ -8,10 +8,13 @@ import {
   Flex,
   Button,
   Checkbox,
+  useColorMode,
 } from "@chakra-ui/react";
 
 export default function Home() {
-  const [todos, setTodos] = React.useState([]);
+  const [todos, setTodos] = React.useState([
+    { id: uuid(), done: false, todo: "다크 테마 넣기" },
+  ]);
   const setTodoByIndex = (index, todo) => {
     setTodos(
       todos.map((originTodo, idx) => {
@@ -19,65 +22,73 @@ export default function Home() {
       })
     );
   };
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container maxW="300px" w="100%" m="100px auto" textAlign="center">
-      <Box position="relative">
-        <Button
-          position="absolute"
-          left="0"
-          bottom="0"
-          size="sm"
-          onClick={() => {
-            setTodos(
-              todos.concat({
-                id: uuid(),
-                done: false,
-                todo: "",
-              })
-            );
-          }}
-        >
-          +
-        </Button>
-        <Heading as="h1">TODO</Heading>
-      </Box>
-      <VStack mt="40px" as="ol">
-        {todos.map(({ id, done, todo }, index) => (
-          <Flex id={id} as="li" textAlign="left" w="100%" alignItems="center">
-            <Checkbox
-              size="lg"
-              p="0"
-              mr="16px"
-              height="fit-content"
-              onChange={(e) => {
-                setTodoByIndex(index, {
-                  done: e.target.checked,
-                  id,
-                  todo,
-                });
-              }}
-            />
-            <Input
-              placeholder="input todo ..."
-              border="none"
-              borderRadius="0"
-              value={todo}
-              _focus={{
-                boxShadow: "0 1px #999",
-              }}
-              textDecoration={done ? "line-through" : "none"}
-              onChange={(e) => {
-                setTodoByIndex(index, {
-                  todo: e.target.value,
-                  id,
-                  done,
-                });
-              }}
-            />
-          </Flex>
-        ))}
-      </VStack>
-    </Container>
+    <>
+      <Button onClick={toggleColorMode} mt="12px" ml="12px">
+        {" "}
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+      <Container maxW="300px" w="100%" m="100px auto" textAlign="center">
+        <Box position="relative">
+          <Button
+            position="absolute"
+            left="0"
+            bottom="0"
+            size="sm"
+            onClick={() => {
+              setTodos(
+                todos.concat({
+                  id: uuid(),
+                  done: false,
+                  todo: "",
+                })
+              );
+            }}
+          >
+            +
+          </Button>
+          <Heading as="h1">TODO</Heading>
+        </Box>
+        <VStack mt="40px" as="ol">
+          {todos.map(({ id, done, todo }, index) => (
+            <Flex id={id} as="li" textAlign="left" w="100%" alignItems="center">
+              <Checkbox
+                size="lg"
+                p="0"
+                mr="16px"
+                height="fit-content"
+                onChange={(e) => {
+                  setTodoByIndex(index, {
+                    done: e.target.checked,
+                    id,
+                    todo,
+                  });
+                }}
+              />
+              <Input
+                placeholder="input todo ..."
+                border="none"
+                borderRadius="0"
+                value={todo}
+                _focus={{
+                  boxShadow: "0 1px #999",
+                }}
+                textDecoration={done ? "line-through" : "none"}
+                onChange={(e) => {
+                  setTodoByIndex(index, {
+                    todo: e.target.value,
+                    id,
+                    done,
+                  });
+                }}
+              />
+            </Flex>
+          ))}
+        </VStack>
+      </Container>
+    </>
   );
 }
 
